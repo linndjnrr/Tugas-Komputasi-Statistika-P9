ff9f4509ec73d901608e0a67852f3041ed4c0a65
Lintang Djenar <lintangdjenar2019@gmail.com>
2026-05-14 09:59:14 +0700

    Update: Statistik untuk Variabel MPG

--- Tugas1_Komstat_P9_LintangDjenar.R
+++ Tugas1_Komstat_P9_LintangDjenar.R
@@ -10,3 +10,14 @@ head(mtcars)
 
 # Statistik Deskriptif
 summary(mtcars)
+
+# Menghitung Statistik untuk Variabel MPG
+mean_mpg <- mean(mtcars$mpg)   # Rata-rata
+sd_mpg   <- sd(mtcars$mpg)     # Standar Deviasi
+max_mpg  <- max(mtcars$mpg)     # Nilai Maksimum
+min_mpg  <- min(mtcars$mpg)     # Nilai Minimum
+
+print(paste("Rata-rata MPG:", round(mean_mpg, 2)))
+print(paste("Standar Deviasi MPG:", round(sd_mpg, 2)))
+print(paste("Nilai Maksimum MPG:", max_mpg))
+print(paste("Nilai Minimum MPG:", min_mpg))

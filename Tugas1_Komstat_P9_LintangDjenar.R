eea06615daf17760a2b3ddbd34a49fc5625ae44f
Lintang Djenar <lintangdjenar2019@gmail.com>
2026-05-14 09:53:25 +0700

    Statistik Deskriptif

---
+++ Tugas1_Komstat_P9_LintangDjenar.R
@@ -0,0 +1,12 @@
+# TUGAS 1: RINGKASAN INTEGRASI RSTUDIO DAN GITHUB
+# Nama  : Lintang Djenar Mahesa Ayu
+# NIM   : 1314624005
+
+# Memuat dataset
+data(mtcars)
+
+# Menampilkan 6 data pertama (Head)
+head(mtcars)
+
+# Statistik Deskriptif
+summary(mtcars)
